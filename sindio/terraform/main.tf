06378ecbf1ebed44a3be2e04c783e691de201866
Jordan Mafumbo <268431929+unidentfied@users.noreply.github.com>
2026-07-08 22:35:17 +0300

    fix(terraform): add missing kubectl provider declaration

--- sindio/terraform/main.tf
+++ sindio/terraform/main.tf
@@ -20,6 +20,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
   }
   backend "s3" {
     bucket         = "sindio-tfstate"
